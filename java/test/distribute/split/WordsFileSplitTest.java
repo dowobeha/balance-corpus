53ce2fb9f54b2795a890c1b3267b13f9b85094fd
Lane Schwartz <dowobeha@gmail.com>
2012-02-16 16:18:00 -0500

    Added unit tests for Java implementation of naive split, histogram split, and words split

---
+++ java/test/distribute/split/WordsFileSplitTest.java
@@ -0,0 +1,77 @@
+package distribute.split;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit tests.
+ * 
+ * @author Lane Schwartz
+ */
+public class WordsFileSplitTest {
+
+    /** Number of parts into which the sample file should be split. */
+    private static final int parts = 16;
+    
+    /** Total number of lines in the sample corpus. */
+    private static final int totalLines = 89;
+
+    /** Path to the sample file to split. */
+    private static final String filePath = "../sample/corpus/udhr.en";
+
+    private static final String outputPrefix = "../sample/words/out.";
+
+    private static final String outputIndexPrefix = "../sample/words/index.";
+
+    @Test
+    public void testBalance() {
+
+	Split splitter = null;
+	try {
+	    splitter = new WordsFileSplit(filePath,parts);    
+	} catch (FileNotFoundException e) {
+	    Assert.fail("Input file not found. It should have been located at " + filePath);
+	}
+
+	Map<Integer,Integer> expectedPartForLine = new HashMap<Integer,Integer>();
+
+	for (int part=1; part<=parts; part+=1) {
+
+	    String indexFilePath = 
+		(part<10) ? 
+		outputIndexPrefix + "0" + part :
+		outputIndexPrefix       + part ;
+		
+	    try {
+		Scanner scanner = new Scanner(new File(indexFilePath));
+
+		while (scanner.hasNextInt()) {
+
+		    int value = scanner.nextInt();
+		    expectedPartForLine.put(value,part);
+
+		}
+	    } catch (FileNotFoundException e) {
+		Assert.fail("Index file not found. It should have been located at " + indexFilePath);
+	    }
+
+	}
+
+
+	for (int line=1; line<=totalLines; line+=1) {
+
+	    int expectedPart = expectedPartForLine.get(line);
+	    int actualPart   = splitter.partitionForLine(line);
+	    Assert.assertEquals(actualPart,expectedPart,"Expected line " + line + " to be in partition " + expectedPart + ", but it was actually in partition " + actualPart);
+
+	}
+
+    }
+	
+}
